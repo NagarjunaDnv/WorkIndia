90777ec791c76a6abbae76f6fb24d138efd4b1b4
Nagarjuna DNV <dnvnagarjunaaa@gmail.com>
2020-10-19 22:20:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const express = require('express');
+const app =express();
+
+
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'abc123',
+    port : '3306',
+    database : 'workIndia'
+});
+
+
+db.connect((err)=>{
+    if(err){
+        console.log("Error connecting to database", err.sqlMessage);
+    }
+    else{
+        console.log("Database connection successfull");
+    }
+})
+
+app.post('/app/user', registrationHandler);
+app.post('/app/user/auth', loginHandler);
+
+
+const PORT = process.env.PORT | 4000;
+
+app.listen(PORT,()=>{
+    console.log(`Server started on http://localhost:${PORT}`);
+})
+
+
+function registrationHandler(req,res){
+    const { username, password } = req.body;
+
+    console.log(username, password);
+
+    if(!username && !password){
+        res.json({
+            status: 'Account creation failed',
+            message: 'Username and Password are required'
+        })
+    }
+
+    if(!username){
+        res.json({
+            status: 'Account creation failed',
+            message: 'Username is required'
+        })
+    }
+
+    if(!password){
+        res.json({
+            status: 'Account creation failed',
+            message: 'Password is required'
+        })
+    }
+
+    const QUERY = `INSERT INTO workIndia (username, password)
+                    VALUES (${username}, ${password})
+                    `;
+
+    db.query(QUERY, (err, rows, fields)=>{
+
+        if(err){
+            res.json({
+                status : 'Account creation failed'
+            })
+        }
+
+        res.json({
+            status : 'Account creation succesfull'
+        })
+    })
+}
+
+
+function loginHandler(req,res){
+    const { username, password } = req.body;
+
+    console.log(username);
+
+    if(!username && !password){
+        res.json({
+            status: 'Login failed',
+            message: 'Username and Password are required'
+        })
+    }
+
+    if(!username){
+        res.json({
+            status: 'Login failed',
+            message: 'Username is required'
+        })
+    }
+
+    if(!password){
+        res.json({
+            status: 'Login failed',
+            message: 'Password is required'
+        })
+    }
+
+    const QUERY = `SELECT users.user_id from users where users.username = ${username} and users.password = ${password} 
+                    `;
+
+    db.query(QUERY, (err, rows, fields)=>{
+
+        if(err){
+            res.json({
+                status : 'Login failed'
+            })
+        }
+
+        res.json({
+            status : 'Login succesfull',
+            id : rows[0]['user_id']
+        })
+    })
+}
